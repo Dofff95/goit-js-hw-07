63ed100fe9c8e78a6edab9c6887a775227e9ec4c
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-09-15 21:35:53 +0300

    1,1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", createMurkup(galleryItems));
+function createMurkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery__item">
+        <img src="${preview}" alt="${description}" class="gallery__image" data-source="${original}"/>
+         </li>`
+    )
+    .join("");
+}
+
+galleryList.addEventListener("click", selectImg);
+function selectImg(evt) {
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  console.log(evt.target);
+}
+const instance = basicLightbox.create(
+  `
+      <img src="" width="1280" height="auto">
+  `,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", escKeyPress);
+    },
+  }
+);
+function escKeyPress(evt) {
+  if (evt.code !== "Escape") return;
+  instance.close();
+}
+
+galleryList.addEventListener("click", fullImgClick);
+function fullImgClick(evt) {
+  evt.preventDefault();
+  const datasetSource = evt.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector("img").src = datasetSource;
+  instance.show();
+}
